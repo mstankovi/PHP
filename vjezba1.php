da9066c08e1b2b569398cfdf73f19f1d6dc1ef43
mstankovi <96198246+mstankovi@users.noreply.github.com>
2026-05-01 13:58:17 +0200

    Add files via upload

---
+++ vjezba1.php
@@ -0,0 +1,25 @@
+<?php
+    
+       $naslov = "Moj prvi PHP dokument";
+       $autor = "Mia Stanković";
+       
+       echo "<h1> $naslov </h1>";
+       echo "<p> Ovu stranicu izradila je <b>$autor</b>. </p>";
+       echo "<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1' target='_blank'> Klikni me.</a>";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<!-- Naziv datoteke: vjezba1.php -->
