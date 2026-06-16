cb4f9adc9c3962007e2cf2aa7ba7794ffb58513b
mstankovi <96198246+mstankovi@users.noreply.github.com>
2026-06-16 22:00:24 +0200

    Add files via upload

---
+++ vjezba 15.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+Vježba 15 - ALTER TABLE
+
+USE vjezba14;
+
+ALTER TABLE users
+ADD email VARCHAR(100) NOT NULL,
+ADD phone VARCHAR(30);
+
+ALTER TABLE users
+MODIFY ime VARCHAR(50) NOT NULL,
+MODIFY prezime VARCHAR(50) NOT NULL;
+
+ALTER TABLE users
+ADD UNIQUE (email);
+
+*/
+
+?>
